70e04eced2715dc446a11a188f903f6861085c9e
StanislawOlech <stanislaw.olech.of@interia.pl>
2026-03-08 09:40:29 +0100

    feat: implement PID controller for landing and refactor landing logic

--- main.py
+++ main.py
@@ -1,101 +1,29 @@
 from djitellopy import Tello
-import cv2
-import cv2.aruco as aruco
 import numpy as np
-import time
-
-def is_safe_to_land(frame):
-    """
-    Landing is safe ONLY if ArUco ID 13 is detected.
-    """
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # --- ArUco Detection ---
-    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-    parameters = aruco.DetectorParameters()
-    detector = aruco.ArucoDetector(aruco_dict, parameters)
-
-    corners, ids, rejected = detector.detectMarkers(gray)
-
-    if ids is not None:
-        ids = ids.flatten()
-        if 13 in ids:
-            return True
-
-    return False
-
-def tello_safe_landing(tello):
-    frame_read = tello.get_frame_read()
-    frame = frame_read.frame
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # --- ArUco Detection ---
-    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-    parameters = aruco.DetectorParameters()
-    detector = aruco.ArucoDetector(aruco_dict, parameters)
-
-    corners, ids, rejected = detector.detectMarkers(gray)
-
-    if ids is not None:
-        for i, marker_id in enumerate(ids.flatten()):
-            if marker_id != 13:
-                continue
-            marker_corners = corners[i][0]
-
-            # Optional: get center pixel
-            center_x = int(marker_corners[:, 0].mean())
-            center_y = int(marker_corners[:, 1].mean())
-            print("Center:", (center_x, center_y))
-
-    tello.land()
+from safe_landing import search4landing_place, Landing_Search_Params
 
 
-step_size = 20
-max_size  = 100
-move_size = step_size
-has_move_size_changed = False
+def main():
+    landing_search_params = Landing_Search_Params(step_size=20,
+                                                  max_size=100)
 
-safe2land = False
+    tello = Tello()
+    tello.connect()
 
-tello = Tello()
-tello.connect()
-
-tello.streamon()
-tello.set_video_direction(1)
-frame_read = tello.get_frame_read()
+    tello.streamon()
+    tello.set_video_direction(1)
 
 
-tello.takeoff()
+    tello.takeoff()
 
-while not safe2land:
-    frame_read = tello.get_frame_read()
-    frame = frame_read.frame
-
-    if frame is None:
-        continue
-
-    safe2land = is_safe_to_land(frame)
-
-
-    if safe2land:
-        print("Safe to land.")
-        tello_safe_landing()
-
-    tello.move_forward(move_size)
-
-    tello.rotate_counter_clockwise(90)
-
-    if has_move_size_changed:
-        move_size += step_size
-        print(f"new search size:{move_size}")
-
-    has_move_size_changed = not has_move_size_changed
-
-    if move_size > max_size:
-        print("1 sq meter searched - emergency landing")
+    try:
+        search4landing_place(tello=tello,
+                             lsp=landing_search_params)
+    except:
         tello.land()
+        print("emergency landing occurred")
 
-tello.land() # redundant command
-tello.streamoff()
+    tello.streamoff()
+
+if __name__ == "__main__":
+    main()
---
+++ safe_landing.py
@@ -0,0 +1,151 @@
+import cv2
+import cv2.aruco as aruco
+import time
+from pid import PID
+
+
+class Landing_Search_Params:
+    def __init__(self,
+                 max_search_size,
+                 step_size,
+                 clockwise=False):
+        self.max_search_size = max_search_size
+        self.step_size = step_size
+        self.clockwise = clockwise
+
+
+def find_aruco(tello, aruco_num=13):
+    frame_read = tello.get_frame_read()
+    frame = frame_read.frame
+
+
+    h, w, _ = frame.shape
+    frame_center = (w // 2, h // 2)
+
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # --- ArUco Detection ---
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    parameters = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(aruco_dict, parameters)
+
+    corners, ids, _ = detector.detectMarkers(gray)
+
+    if ids is None:
+        return None
+
+    for i, marker_id in enumerate(ids.flatten()):
+        if marker_id != aruco_num:
+            continue
+        marker_corners = corners[i][0]
+
+        center_x = int(marker_corners[:, 0].mean())
+        center_y = int(marker_corners[:, 1].mean())
+
+        return (center_x - frame_center[0], center_y - frame_center[1]) # TODO make sure the coordinates are not mismatched
+
+    return None
+
+
+def pid_landing(tello,
+                aruco_id=13,
+                k_p=0.25,
+                k_i=0.0,
+                k_d=0.0): # TODO adjust params
+
+    pid_x = PID(k_p, k_i, k_d, limit=20)
+    pid_y = PID(k_p, k_i, k_d, limit=20)
+
+
+    # TODO adjust params
+    descent_speed = 10
+    landing_threshold = 20  # in pixels
+
+    prev_time = time.time()
+
+    while True:
+
+        aruco_pos_error = find_aruco(tello, aruco_id)
+
+        if aruco_pos_error is None:
+            raise("Aruco symbol lost")
+
+        error_x, error_y = aruco_pos_error
+
+
+        now = time.time()
+        dt = now - prev_time
+        prev_time = now
+
+
+        vx = pid_y.update(error_y, dt) * -1
+        vy = pid_x.update(error_x, dt)
+
+
+        # descent only if roughly centered
+        if abs(error_x) < landing_threshold and abs(error_y) < landing_threshold:
+            vz = -descent_speed
+        else:
+            vz = 0
+
+
+        tello.send_rc_control(vy, vx, vz, 0)
+
+
+        print(f"errorX:{error_x} errorY:{error_y}")
+
+
+        # final landing
+        if abs(error_x) < 10 and abs(error_y) < 10 and tello.get_height() < 20:
+            print("Landing")
+            tello.land()
+            break
+
+
+def search4landing_place(tello, lsp):
+    aruco_pos_error = None
+    change_move_size = False
+    current_move = lsp.step_size
+
+    while aruco_pos_error is None:
+        frame_read = tello.get_frame_read()
+        frame = frame_read.frame
+
+        if frame is None:
+            continue
+
+
+        aruco_pos_error = find_aruco(frame)
+
+
+        if aruco_pos_error is not None:
+            print("Safe to land.")
+            try:
+                pid_landing(tello)
+            except:
+                print("Aruco lost during landing return to the search")
+                continue
+            return
+
+
+        # move to the next position
+        tello.move_forward(current_move)
+
+        if lsp.clockwise:
+            tello.rotate_clockwise(90)
+        else:
+            tello.rotate_counter_clockwise(90)
+
+
+        # adjust moving distance every second iteration
+        if change_move_size:
+            current_move += lsp.step_size
+            print(f"new search size:{current_move}")
+
+        change_move_size = not change_move_size
+
+
+        # check if inbounds
+        if current_move > lsp.max_size:
+            raise("Landing site not found in the desired size — initiate emergency landing")
